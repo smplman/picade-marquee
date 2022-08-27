0f5504ed149c9ec3096c50f343200e7317387443
smplman <smp4488@aol.com>
2022-08-26 23:17:29 -0400

    fixing strip order and controlling button rgb via gpio

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const ws281x = require('@gbkwiatt/node-rpi-ws281x-native');
-
+const gpio = require('rpi-gpio');
 // on / off
 const state = process.argv[2];
 
 console.log('setting state: ', state);
+
+// Button RGB
+gpio.setup(16, gpio.DIR_OUT, write);
+
+function write(err) {
+    if (err) throw err;
+    gpio.write(16, state === 'on' ? true : false, function(err) {
+        if (err) throw err;
+        console.log('Gpio 16 Buttons', state);
+	return;
+    });
+}
+
+// RGB Marquee
 const color = state === 'on' ? 0xffffff : 0x00;
 
 const options = {
@@ -29,7 +43,7 @@ for (let i = 0; i < channel.count; i++) {
     colorsArray[i] = color;
   } else {
     // set left side to red
-    if (i <= 8 || i > 24 && i < 32 || i >= 32 && i <= 40) {
+    if (i < 8 || i > 23 && i < 32 || i >= 32 && i <= 40) {
       colorsArray[i] = 0xff0000;
     } else {
       // set right side to blue
@@ -39,3 +53,8 @@ for (let i = 0; i < channel.count; i++) {
 }
 
 ws281x.render();
+console.log('done');
+// we need to manually exit after some time due to how rpi-gpio works
+setTimeout(() => {
+	process.exit();
+}, 1000);
